5667b208c182bd1b55fbd590995451358a3473b3
Hello-World <Mohammedazin>
2026-09-03 14:50:46 +0530

    backend

--- secure-file-backend/src/routes/log.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { requireAuth } from "../middleware/auth.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { getLogs } from "../controllers/log.controller.js";
-
-const router = Router();
-router.get("/", requireAuth, asyncHandler(getLogs));
-export default router;
